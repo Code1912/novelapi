92f159fd03dd409dfcee01b41a7b99ea1838eca1
Code1912 <code1912@163.com>
2016-12-05 23:34:14 +0800

    modify  biz

--- biz/luoqiu.js
+++ biz/luoqiu.js
@@ -6,25 +6,37 @@ let $=require("cheerio");
 let he=require("he")
 let iconv = require('iconv-lite');
 let BufferHelper = require('bufferhelper');
+const  type=1;
 class LuoQiu {
     static search(req, res) {
         console.log(req.params);
-        let keyWord = req.param("keyword", "");
+        let keyword = req.param("keyword", "");
+        console.log(keyword)
         let pageIndex = req.param("pageIndex", "");
         let pageIndexStr = pageIndex > 1 ? `&p=${pageIndex - 1}` : "";
-        http.get(`http://zhannei.baidu.com/api/customsearch/searchwap?q=${encodeURIComponent(keyWord)}${pageIndexStr}&s=17782022296417237613&srt=def&nsid=0`, (res1) => {
+        let url=`http://zhannei.baidu.com/api/customsearch/searchwap?q=${encodeURIComponent(keyword)}${pageIndexStr}&s=17782022296417237613&srt=def&nsid=0`;
+        console.log(url)
+        http.get(url, (res1) => {
             let html = '';
             res1.on('data', function (data) {
                 html += data;
             });
             res1.on('end', function () {
                 let data = JSON.parse(html);
-                res.send({
+                let retData={
                     success: data.results ? true : false,
                     pageIndex: data.curpage,
                     totalCount: data.totalNum,
-                    resultList: data.results
-                });
+                    resultList: data.results||[]
+                };
+                if(retData.resultList.length>0){
+                    retData.resultList.forEach(n=>{
+                        n.current_url=(n.listPage_url||[]).length>0?n.listPage_url[0]:"";
+                        n.type=type;
+                    })
+                }
+                console.log(retData);
+                res.send(retData);
 
             });
             this.errorListen(res1,res)
@@ -42,11 +54,15 @@ class LuoQiu {
                 let array = [];
                 let html = iconv.decode(bufferHelper.toBuffer(), 'GBK')
                 let chapterList = $(html).find("#defaulthtml4>table .dccss a") || [];
+                let index=0;
                 chapterList.each((i, p) => {
                     let obj = $(p);
+                    index++;
                     array.push({
                         title: obj.text(),
-                        url: `http://www.luoqiu.com${obj.attr("href")}`
+                        url: `http://www.luoqiu.com${obj.attr("href")}`,
+                        chapter_index:index,
+                        type:type
                     })
                 });
 
@@ -68,12 +84,15 @@ class LuoQiu {
             });
             res1.on('end', function () {
                 let array = [];
-                let html = iconv.decode(bufferHelper.toBuffer(), 'GBK');
+                let html = iconv.decode(bufferHelper.toBuffer(), 'GBK')||"";
+                html=html.replace(/(<br>)|(<br\/>)|(<br \/>)|((<br >))/ig,"\n");
+               // html=html.replace(/&nbsp;/ig,"\t")
                 let htmlJQ = $(html);
-
+                console.log(html)
                 var result={
                     title: htmlJQ.find(".bname_content").text(),
-                    content: he.decode(htmlJQ.find("#content").html())||htmlJQ.find("#content").text()
+                    content: he.decode(htmlJQ.find("#content").text()),
+                    type:type
                 };
                 res.send({
                     success: result.content?true:false,
@@ -85,7 +104,7 @@ class LuoQiu {
     }
     static  errorListen(httpRes,res){
         httpRes.on("error", () => {
-            res1.send(
+            res.send(
                 {
                     success: false,
                     message:"get novel error"
